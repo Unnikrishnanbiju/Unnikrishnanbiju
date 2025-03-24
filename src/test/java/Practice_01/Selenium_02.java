7cec9f47f06d576b4cd2b25f2ac8f004f21dd8b2
unnikrishnanbiju <unni1995biju@gmail.com>
2025-03-24 20:55:36 +0530

    initial commit

---
+++ src/test/java/Practice_01/Selenium_02.java
@@ -0,0 +1,10 @@
+package Practice_01;
+
+import org.testng.annotations.BeforeClass;
+
+public class Selenium_02 {
+@BeforeClass
+public void TestCase(){
+
+    }
+}
---
+++ src/test/java/Practice_01/Selenium_03.java
@@ -0,0 +1,54 @@
+package Practice_01;
+
+import io.qameta.allure.Owner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Selenium_03 {
+    ChromeDriver driver;
+
+    @BeforeTest
+    public void OpenBrowser(){
+        driver = new ChromeDriver();
+
+    }
+
+
+
+    @Owner("Unni")
+    @Test
+    public void testBrowser(){
+driver.get("https://app.vwo.com");
+driver.manage().window().maximize();
+ driver.getTitle();
+
+        List<WebElement> button_list = driver.findElements(By.tagName("button"));
+        System.out.println(button_list);
+        for (WebElement button: button_list){
+            System.out.println(button.getText());
+
+
+        }
+        WebElement link_text_start_tril = driver.findElement(By.linkText("Start a free trial"));
+        link_text_start_tril.click();
+    }
+
+
+    @AfterTest
+    public void CloseBrowser() {
+        try {
+            Thread.sleep(Long.parseLong("3000"));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
+
+
